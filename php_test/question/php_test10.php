3f1f93013d448068ccaf5294e75fe9648efce58a
yosuke0715 <yosuke.frontend@gmail.com>
2021-08-06 20:57:18 +0900

    更新しました

---
+++ php_test/question/php_test10.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP10</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/common.css">
+</head>
+<body>
+    <header>
+        <h1>PHP10【シンプルメモ帳を作る】</h1>
+    </header>
+    <div class="test_area">
+        <p class="ly_module_text">条件</p>
+        <p class="ly_module_text">①　メモ追加機能を実装する</p>
+        <p class="ly_module_text">②　メモ削除機能を実装する</p>
+        <p class="ly_module_text">③　メモ内容編集機能を実装する</p>
+        <p class="ly_module_text">④　データ管理は全てDBで行う</p>
+    </div>
+</body>
+</html>
